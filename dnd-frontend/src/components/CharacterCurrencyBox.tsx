71f00fa71845a3462d98a3d382673037e44da1f9
Dobos Patrik <d.patrik0610@gmail.com>
2025-12-07 00:31:33 +0100

    rm 0 amount currencies from view.

--- dnd-frontend/src/components/CharacterCurrencyBox.tsx
+++ dnd-frontend/src/components/CharacterCurrencyBox.tsx
@@ -1,14 +1,14 @@
 import { Group, Text } from "@mantine/core";
 import CustomBadge from "./common/CustomBadge";
 import { SectionColor } from "../types/SectionColor";
-import { randomId } from "@mantine/hooks";
 import { useCharacterStore } from "../store/useCharacterStore";
 import { IconCoin } from "@tabler/icons-react";
 
 export function CharacterCurrencyBox() {
-    const currencies = useCharacterStore((state) => state.character?.currencies)!;
+    const currencies = useCharacterStore((state) => state.character?.currencies ?? []);
+    const visibleCurrencies = currencies.filter((c) => (c.amount ?? 0) > 0);
 
-    const hasAnyCurrencies = currencies.length > 0;
+    const hasAnyCurrencies = visibleCurrencies.length > 0;
     function currencyColor (currencyCode: string) {
         switch (currencyCode) {
             case "gp": return SectionColor.Yellow;
@@ -19,9 +19,9 @@ export function CharacterCurrencyBox() {
 
     return (
         <Group m={5} p={5} w="100%" align="center" justify="center" wrap="wrap">
-            {hasAnyCurrencies ? currencies.map((currency) => (
+            {hasAnyCurrencies ? visibleCurrencies.map((currency) => (
                 <CustomBadge
-                key={randomId()}
+                key={`${currency.type}-${currency.currencyCode}`}
                 label={`${currency.amount} ${currency.currencyCode}`}
                 color={currencyColor(currency.currencyCode)}
                 variant = "light"
