29be9dd86c0048372e79f7670d2d4fda35cfd791
Dobos Patrik <d.patrik0610@gmail.com>
2025-12-07 00:49:58 +0100

    introducing path aliases

--- dnd-frontend/vite.config.ts
+++ dnd-frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,16 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@types': path.resolve(__dirname, 'src/types'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
     host:'0.0.0.0',
     port:3000,
